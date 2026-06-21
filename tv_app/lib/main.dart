6bba5fb24cee9319338946f84885fb2dc9458de5
Tan Giao <0306221415@caothang.edu.vn>
2026-06-21 17:47:43 +0700

    Complete TV app framework features

--- tv_app/lib/main.dart
+++ tv_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter/services.dart';
 import 'src/app.dart';
+import 'dart:io';
 import 'src/core/services/restart_service.dart';
 import 'src/core/services/app_lifecycle_service.dart';
 import 'src/core/services/update_service.dart';
@@ -10,7 +11,13 @@ import 'src/core/services/update_service.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  if (Platform.isAndroid) {
   await RestartService.initialize();
+
+    WidgetsBinding.instance.addObserver(
+      AppLifecycleService(),
+    );
+  }
 
   WidgetsBinding.instance.addObserver(
     AppLifecycleService(),
